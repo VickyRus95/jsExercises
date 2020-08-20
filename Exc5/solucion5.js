69ead1aa853e2b4498cdd07897839ab07ca3164e
VickyRus95 <victoriarustraducciones@gmail.com>
2020-08-20 18:10:11 -0300

    Exc5 complete

---
+++ Exc5/solucion5.js
@@ -0,0 +1,12 @@
+function giveMeSomething (a) {
+return "Something"+ " " + a
+
+}
+document.write ("<br>")
+document.write (giveMeSomething ("about her makes me crazy."))
+document.write ("<br>")
+document.write ("<br>")
+document.write (giveMeSomething ("about his voice quickened her pulse."))
+document.write ("<br>")
+document.write ("<br>")
+document.write (giveMeSomething ("is behind you!!!"))
