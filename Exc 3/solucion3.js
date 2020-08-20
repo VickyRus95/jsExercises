f8e93dbfe9747b15a9a75875785aecfdcf3f8cb1
VickyRus95 <victoriarustraducciones@gmail.com>
2020-08-20 09:03:48 -0300

    Exc. complete

---
+++ Exc 3/solucion3.js
@@ -0,0 +1,9 @@
+function addition (num){
+let resultado= num + 1
+return resultado
+}
+document.write ("El numero 5 + 1 da= " + addition (5))
+document.write("<br>")
+document.write ("El numero 74 + 1 da= " + addition (74))
+document.write("<br>")
+document.write ("El numero 56 + 1 da= " + addition (56))
--- Exc2/solucion2.js
+++ Exc2/solucion2.js
@@ -1,12 +1,12 @@
 
 
-function minutosSegundos (min){
+function minutesToSeconds (min){
 var resultado= min* 60
  
 return resultado}
 
-document. write ("Conversion de 5 minutos a segundos : " + minutosSegundos(5));
+document. write ("Conversion de 5 minutos a segundos : " + minutesToSeconds(5));
 document.write ("<br>")
-document. write ("Conversion de 8 minutos a segundos : " + minutosSegundos(8))
+document. write ("Conversion de 8 minutos a segundos : " + minutesToSeconds(8))
 document.write ("<br>")
-document. write ("Conversion de 9 minutos a segundos : " + minutosSegundos(9))
+document. write ("Conversion de 9 minutos a segundos : " + minutesToSeconds(9))
