4d7400c665073f0b163ce5bb45fa26653d2c64ef
VickyRus95 <victoriarustraducciones@gmail.com>
2020-08-21 10:43:23 -0300

    Exc 7 complete

---
+++ Exc7/solucion7.js
@@ -0,0 +1,11 @@
+
+let valor= prompt ("Ingrese un valor")
+
+function hoursToSeconds (valor) {
+
+    return valor*3600
+    }
+    
+    document.write ("<br>")
+    document.write ("Conversion de " + valor + " horas a segundos = " + hoursToSeconds (valor))
+    document.write ("<br>")
