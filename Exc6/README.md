9706e8d28ef83d85aa2e577f4d457b708597ebea
VickyRus95 <victoriarustraducciones@gmail.com>
2020-08-21 10:19:06 -0300

    Exc6 complete

---
+++ Exc6/README.md
@@ -0,0 +1,8 @@
+Convert Hours into Seconds
+Write a function that converts hours into seconds.
+Examples
+howManySeconds(2) ➞ 7200
+
+howManySeconds(10) ➞ 36000
+
+howManySeconds(24) ➞ 86400
---
+++ Exc6/solucion6.js
@@ -0,0 +1,11 @@
+function hoursToSeconds (hour) {
+
+return hour*3600
+
+}
+document.write ("<br>")
+document.write ("Conversion de 2 horas a segundos = " + hoursToSeconds (2))
+document.write ("<br>")
+document.write ("Conversion de 16 horas a segundos = " + hoursToSeconds (16))
+document.write ("<br>")
+document.write ("Conversion de 9 horas a segundos = " + hoursToSeconds (9))
