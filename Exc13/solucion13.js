8631a12b1b6ab33368d635a266c149d8d3c116ae
VickyRus95 <victoriarustraducciones@gmail.com>
2020-08-24 13:22:46 -0300

    Exc 13 complete

--- Exc13/solucion13.js
+++ Exc13/solucion13.js
@@ -1,17 +1,18 @@
 function findRemainder (num1, num2){
-    result= num1%num2 
+    result = num1 % num2 
     return result
 }
 let remainder = findRemainder (1, 3)
 document.write ("El resultado es "+ remainder)
 document.write ("<br>")
-
-let remainder2= findRemainder (3, 4)
+document.write ("<br>")
+let remainder2 = findRemainder (3, 4)
 document.write ("El resultado es "+ remainder2)
 document.write ("<br>")
-
-let remainder3= findRemainder (-9, 45)
+document.write ("<br>")
+let remainder3 = findRemainder (-9, 45)
 document.write ("El resultado es "+ remainder3)
 document.write ("<br>")
-let remainder4= findRemainder (5, 5)
+document.write ("<br>")
+let remainder4 = findRemainder (5, 5)
 document.write ("El resultado es "+ remainder4)
