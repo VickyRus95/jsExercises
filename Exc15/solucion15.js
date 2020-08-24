2085124ebeed8740edc3be4621974e41616940c0
VickyRus95 <victoriarustraducciones@gmail.com>
2020-08-24 17:31:36 -0300

    Exc 14 omplete

---
+++ Exc15/solucion15.js
@@ -0,0 +1,15 @@
+function lessThanOrEqualToZero (num){
+if (num <= 0){
+    document.write ("TRUE")
+}
+else {document.write ("FALSE")
+}
+}
+document.write ("<br>")
+lessThanOrEqualToZero(8)
+document.write ("<br>")
+lessThanOrEqualToZero(-1)
+document.write ("<br>")
+lessThanOrEqualToZero(3)
+document.write ("<br>")
+lessThanOrEqualToZero(0)
