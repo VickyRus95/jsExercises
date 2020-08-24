b4c9e69bdc36313b44e6f3c2e4510a41c3d13fab
VickyRus95 <victoriarustraducciones@gmail.com>
2020-08-24 14:40:20 -0300

    Exc14 complete

---
+++ Exc14/solucion14.js
@@ -0,0 +1,11 @@
+function operatorAnd (a,b){
+    if(a===true && b===true){
+        document.write ("A and B are TRUE")
+    }
+    else {"One of them is not TRUE"}
+
+
+}
+operatorAnd (true, true)
+operatorAnd (true, false)
+operatorAnd (false, true)
