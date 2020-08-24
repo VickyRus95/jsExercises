46004caaa8c2b15211d2071427d8ae8d0303cc07
VickyRus95 <victoriarustraducciones@gmail.com>
2020-08-24 11:49:17 -0300

    Exc10 complete

---
+++ Exc10/solucion10.js
@@ -0,0 +1,11 @@
+
+document.write ("Exc 10 ")
+document.write ("<br>")
+document.write ("<br>")
+document.write ("<br>")
+
+function getFirstElement(arr){
+    if (!arr.length){document.write("Usted no ha ingresado ningún valor.")}
+else{document.write("Su array es "+ arr)}
+}
+getFirstElement ([])
