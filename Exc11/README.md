683da9d562ec558ed31c455055573e36dece2e5d
VickyRus95 <victoriarustraducciones@gmail.com>
2020-08-24 12:23:23 -0300

    Exc11 complete

---
+++ Exc11/README.md
@@ -0,0 +1,12 @@
+Maximum Edge of a Triangle
+
+Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+
+Examples
+nextEdge(8, 10) ➞ 17
+
+nextEdge(5, 7) ➞ 11
+
+nextEdge(9, 2) ➞ 10
+
+(side1 + side2) - 1 = maximum range of third edge.
---
+++ Exc11/solucion11.js
@@ -0,0 +1,20 @@
+document.write ("Exc 11")
+document.write ("<br>")
+document.write ("<br>")
+function maximumRange (side1, side2){
+
+resultado= (side1 + side2) - 1 
+return resultado
+}
+
+let tercerLado= maximumRange (8, 10)
+document.write ("El tercer lado equivale a "+ tercerLado)
+document.write ("<br>")
+document.write ("<br>")
+let tercerLado2= maximumRange (5, 7)
+document.write ("El tercer lado equivale a "+ tercerLado2)
+document.write ("<br>")
+document.write ("<br>")
+let tercerLado3= maximumRange (9, 2)
+document.write ("El tercer lado equivale a "+ tercerLado3)
+
