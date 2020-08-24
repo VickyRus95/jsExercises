896555ecf25ab20b8f8f1b884e4bd74e7444b0cf
VickyRus95 <victoriarustraducciones@gmail.com>
2020-08-24 12:50:01 -0300

    Exc 12 complete

---
+++ Exc12/README.md
@@ -0,0 +1,10 @@
+Find the Perimeter of a Rectangle
+
+Create a function that takes length and width and finds the perimeter of a rectangle.
+
+Examples
+findPerimeter(6, 7) ➞ 26
+
+findPerimeter(20, 10) ➞ 60
+
+findPerimeter(2, 9) ➞ 22
---
+++ Exc12/solucion12.js
@@ -0,0 +1,15 @@
+function perimeterTriangle (side1, side2){
+    resultado= (side1 + side2) *2 
+    return resultado
+
+}
+let perimeter= perimeterTriangle (6, 7)
+document.write ("El perimetro equivale a "+ perimeter)
+document.write ("<br>")
+
+let perimeter2= perimeterTriangle (20, 10)
+document.write ("El perimetro equivale a "+ perimeter2)
+document.write ("<br>")
+
+let perimeter3= perimeterTriangle (2, 9)
+document.write ("El perimetro equivale a "+ perimeter3)
