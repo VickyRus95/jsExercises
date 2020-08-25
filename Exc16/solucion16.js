0a2ea28938336753698abb81b4f156794411c4b6
VickyRus95 <victoriarustraducciones@gmail.com>
2020-08-25 13:11:57 -0300

    Exc 15 complete

---
+++ Exc16/solucion16.js
@@ -0,0 +1,17 @@
+
+function lessThan100 (num1, num2){
+let suma = num1 + num2
+if (suma < 100){return true}
+else {return false}
+
+
+}
+
+document.write ("<br>")
+document.write ("El resultado de la suma es menor que 100? " + lessThan100 (22, 15))
+document.write ("<br>")
+document.write ("El resultado de la suma es menor que 100? " + lessThan100 (83,34))
+document.write ("<br>")
+document.write ("El resultado de la suma es menor que 100? " + lessThan100 (3, 77))
+
+
