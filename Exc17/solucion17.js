2b6ecb0f2a5c99b1a06855e0b4a5ba40a545295a
VickyRus95 <victoriarustraducciones@gmail.com>
2020-08-25 19:50:23 -0300

    Exc17 complete

---
+++ Exc17/solucion17.js
@@ -0,0 +1,22 @@
+function footballPoints (num1, num2, num3){
+let finalResult= (num1 * 3) + num2
+return finalResult
+}
+
+document.write ("<br>")
+document.write ("El resultado final del equipo es " + footballPoints(3, 4, 2)) 
+document.write ("<br>")
+document.write ("El resultado final del equipo es " + footballPoints(5, 0, 2)) 
+document.write ("<br>")
+document.write ("El resultado final del equipo es " + footballPoints(0, 0, 1))
+
+
+
+
+
+
+
+
+
+
+
