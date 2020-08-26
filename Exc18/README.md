1fbfa32ec79357fed7b3027e94c40999920f5cc3
VickyRus95 <victoriarustraducciones@gmail.com>
2020-08-26 12:13:44 -0300

    Exc 18 complete

---
+++ Exc18/README.md
@@ -0,0 +1,10 @@
+Are the Numbers Equal?
+
+Create a function that returns true when num1 is equal to num2; otherwise return false.
+
+Examples
+isSameNum(4, 8) ➞ false
+
+isSameNum(2, 2) ➞  true
+
+isSameNum(2, "2") ➞ false
---
+++ Exc18/solucion18.js
@@ -0,0 +1,15 @@
+document.write ("Exc 18")
+document.write ("<br>")
+function isSameNum (num1, num2){
+if (num1===num2){return true}
+else {return false}
+
+}
+
+document.write ("<br>")
+document.write ("Los numeros son iguales? " +isSameNum(4, 8))
+document.write ("<br>")
+document.write ("Los numeros son iguales? " +isSameNum(2, 2))
+document.write ("<br>")
+document.write ("Los numeros son iguales? " + isSameNum(2, "2"))
+document.write ("<br>")
