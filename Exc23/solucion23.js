0c27855636c30b167505bf9ad32abc4a2ca256b3
VickyRus95 <victoriarustraducciones@gmail.com>
2020-08-28 10:19:38 -0300

    Exc23 complete

---
+++ Exc23/solucion23.js
@@ -0,0 +1,9 @@
+function frames (num1,num2){
+let resultado= (num1 * num2) * 60
+return resultado
+}
+document.write (frames (1,1))
+document.write ("<br>")
+document.write (frames (10,1))
+document.write ("<br>")
+document.write (frames (10,25))
