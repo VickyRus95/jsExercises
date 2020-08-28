a3ad3e8076296d4fca1878dc9dfbdbb68d956d3e
VickyRus95 <victoriarustraducciones@gmail.com>
2020-08-28 12:53:21 -0300

    Exc24 complete

---
+++ Exc24/solucion24.js
@@ -0,0 +1,13 @@
+function matchHouses (step) {
+  let result= (step * 4) + (step + 1)
+    return result
+}
+
+
+document.write (matchHouses(1))
+document.write ("<br>")
+document.write (matchHouses(4))
+document.write ("<br>")
+document.write (matchHouses(87))
+document.write ("<br>")
+document.write (matchHouses(36))
