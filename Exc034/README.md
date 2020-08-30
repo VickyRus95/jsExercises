670897aee483c1477ebb79c5fc9f01001bfca067
VickyRus95 <victoriarustraducciones@gmail.com>
2020-08-30 17:22:33 -0300

    Exc034 complete

---
+++ Exc034/README.md
@@ -0,0 +1,1 @@
+Realizar un programa que pida cargar una fecha cualquiera, luego verificar si dicha fecha corresponde a Navidad (se debe cargar por separado el dia, el mes y el año)
---
+++ Exc034/solucion034.js
@@ -0,0 +1,7 @@
+let fecha = prompt ("Ingrese un dia")
+let mes = prompt ("Ingrese un mes")
+if (fecha == 25 && mes == "diciembre" ) {
+    document.write ("Feliz Navidad!!!")
+} else {
+    document.write ("Hoy no es navidad.")
+}
