835373f01e86ef5bd60e368722303583b2be5c30
VickyRus95 <victoriarustraducciones@gmail.com>
2020-08-30 18:27:23 -0300

    Exc 038 complete

---
+++ Exc038/README.md
@@ -0,0 +1,1 @@
+Mostrar los múltiplos de 8 hasta el valor 500. Debe aparecer en pantalla 8 -16 -24, etc.
---
+++ Exc038/solucion038.js
@@ -0,0 +1,8 @@
+let x;
+x=1;
+while (x<=500)
+{
+  document.write (x * 8);
+  document.write('<br>');
+  x=x+1;
+}
