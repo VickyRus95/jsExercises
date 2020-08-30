905467724d8669c29c1aaecd7274586ab7e9622d
VickyRus95 <victoriarustraducciones@gmail.com>
2020-08-30 15:58:29 -0300

    Exc 029 complete

---
+++ Exc029/solucion029.js
@@ -0,0 +1,7 @@
+let clave1 = prompt ("Ingrese su contraseña")
+let clave2 = prompt ("Ingrese de nuevo su clave")
+if (clave1== clave2) {
+    document.write ("Las claves son iguales")
+} else {
+    document.write ("Las claves son diferentes")
+}
--- Exc26/solucion26.js
+++ Exc26/solucion26.js
@@ -1,10 +1,21 @@
 
-function detectWord(aCharacter)    
-{    
-    if (aCharacter== 'a','b','c','d')
-    {console.log ()};
-}
+function detectWord (charcode){
 
+    if(charCode >= "A".charCodeAt(0) && charCode <= "Z".charCodeAt(0)){
+
+        returnValue = "U";
+    
+    }else if (charCode >= "a".charCodeAt(0) &&
+                charCode <= "z".charCodeAt(0) ){
+        returnValue = "L";
+    }
+
+}
 detectWord("UcUNFYGaFYFYGtNUH") 
 detectWord("bEEFGBuFBRrHgUHlNFYaYr") 
 detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment")
+
+
+
+
+
