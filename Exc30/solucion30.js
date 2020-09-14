899f7ed97da0960dc894e00e9b7333ebcf4e9d15
VickyRus95 <victoriarustraducciones@gmail.com>
2020-09-14 11:05:29 -0300

    Exc30 complete

---
+++ Exc30/solucion30.js
@@ -0,0 +1,12 @@
+function minMax (numbers){
+    var max = Math.max(...numbers);
+    var min= Math.min(...numbers);
+    console.log (min, max)
+   
+}
+
+minMax([1, 2, 3, 4, 5]) 
+
+minMax([2334454, 5]) 
+
+minMax([1])
